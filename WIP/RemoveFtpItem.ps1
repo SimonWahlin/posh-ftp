d8ec856495b1ef3d1be2bf93c80e9e37f166feb3
Simon Wåhlin <simon.wahlin@gmail.com>
2016-02-13 22:42:07 +0100

    Remove-FTPItem now handles recursive removal of folders

--- WIP/RemoveFtpItem.ps1
+++ WIP/RemoveFtpItem.ps1
@@ -5,39 +5,65 @@ Function Remove-FTPItem {
         [System.Net.FtpWebRequest]
         $Session,
         
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true)]
+        [Alias('FullName')]
         [String]
-        $Path
+        $Path,
+        
+        [Parameter(Mandatory=$false)]
+        [Switch]
+        $Recurse
     )
-    try
-    {
-        $RequestItem = Get-FTPItem @PSBoundParameters
-        if($RequestItem.PSISContainer)
-        {
-            $Method = 'RemoveDirectory'
-        }
-        else
-        {
-            $Method = 'DeleteFile'
-        }
-        $Request = $Request = New-FTPSession @PSBoundParameters -Method $Method
-        $FTPResponse = $Request.GetResponse()
-        Write-Verbose -Message $FTPResponse.StatusDescription
-    }
-    catch
-    {
-        throw
-    }
-    finally
+    Process
     {
         try
         {
-            $FTPResponse.Close()
-            $FTPResponse.Dispose()
+            $Target = Get-FTPChildItem -Session $Session -Path $Path -Detailed
+            if(-Not($Target) -or $Target.Name -eq (Split-Path -Path $Path -Leaf))
+            {
+                $RequestItem = Get-FTPItem -Session $Session -Path $Path
+                if($RequestItem.PSISContainer)
+                {
+                    $Method = 'RemoveDirectory'
+                }
+                else
+                {
+                    $Method = 'DeleteFile'
+                }
+                Write-Verbose -Message ('Removing: [{0}]' -f $Path)
+                $Request = $Request = New-FTPSession -Session $Session -Path $Path -Method $Method
+                $FTPResponse = $Request.GetResponse()
+                Write-Verbose -Message $FTPResponse.StatusDescription
+            }
+            else
+            {
+                if($Recurse)
+                {
+                    $null = $PSBoundParameters.Remove('Path')
+                    $Target | Remove-FTPItem @PSBoundParameters
+                    Remove-FTPItem @PSBoundParameters -Path $Path
+                }
+                else
+                {
+                    Write-Warning -Message ('Path: [{0}] is not empty, use -Recurse to remove recursively' -f $Path)
+                }           
+            }
         }
         catch
         {
-            
+            throw
+        }
+        finally
+        {
+            try
+            {
+                $FTPResponse.Close()
+                $FTPResponse.Dispose()
+            }
+            catch
+            {
+                
+            }
         }
     }
 }
